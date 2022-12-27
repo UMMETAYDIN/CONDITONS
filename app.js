057cd0ffa5f813f9c3ebc0a316b6656e67229451
ummet <ummetaydin080@gmail.com>
2022-12-27 12:14:09 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+
+// örnek-1-
+// consolda girilen bir sayının pozitif negatif yad sıfır oldugunu tespit ederek yazdırınız
+
+// const sayi= +prompt("bir sayı giriniz")
+
+// if(sayi>0){
+//     console.log(`${sayi} sayisi pozitif bir sayidir`);
+
+// }
+
+// if (sayi>0) {
+//     console.log(`${sayi} sayisi pozitif bir sayidir`);
+    
+// }else if (sayi<0){
+//     console.log(`${sayi} sayisi negatif bir sayidir`);
+// }else {
+//     console.log(`${sayi} sifira eşittir`);
+// }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// örnek-2-
+// consoleden 3 tansayı alarak bunların en büyüğünü yazdırınız
+
+// 1. yöntem
+
+// const n1=+prompt("1. sayıyı giriniz")
+// const n2=+prompt("2. sayıyı giriniz")
+// const n3=+prompt("3. sayıyı giriniz")
+
+// if (n1>=n2 && n1>=n3) {
+//     console.log(`${n1} is biggest`);
+
+// }else if(n2>=n1 && n2>=n3){
+//     console.log(`${n2} is biggest`);
+
+// } else if(n3>=n1 && n3>=n2){
+//     console.log(`${n3} is biggest`);
+// }
+
+// 2. yöntem
+
+// let biggest =n1
+
+// if(n2>=biggest){
+//     biggest=n2
+// }
+
+// if (n3>=biggest) {
+//     biggest =n3
+    
+// }
+// console.log(`${biggest} is biggest`);
+
+// 3. yöntem
+
+// console.log(`the biggest:${Math.max(n1,n2,n3)}`);
+
+// 5>3 ? confirm("sil"): confirm("silme")
+
+
+// 5>3 ? confirm("sezer"): confirm("ökmen")
+
+// 5>3 ? alert("sezer"): alert("ökmen")
+
+//  const grade=+prompt("please enter your grade")
+//  const result=
+
+//  grade>=50
+//  ? `you are succedful with ${grade}`
+//  : `you are failed with ${grade}`
+//  console.log(result);
+
+
+// const grade=+prompt("please enter your grade:")
+// const result=
+
+// grade>=75
+// ? `you are succesfull with ${grade}`
+// :`you are failed with ${grade}`
+// console.log(result);
